9eaafbd6da790983b6a986f3f95dc11f6ca6f8aa
fujiyamaorange <kaito.bakuman@gmail.com>
2023-01-17 22:42:47 +0900

    assert section

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('Start!');
-console.log('End!');
+console.log('=====Start=====');
+
+// assert
+const wishToSuccess = () => {
+  return Math.random() < 0.5
+}
+console.assert(wishToSuccess(), "失敗しました")
+
+console.log('=====End=====');
