f82b38aa70dcc432bf39f8eebbd5b9c5099f17d4
fujiyamaorange <kaito.bakuman@gmail.com>
2023-01-17 23:10:14 +0900

    log section

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ console.log('=====Start=====');
 const wishToSuccess = () => {
   return Math.random() < 0.5
 }
-console.assert(wishToSuccess(), "失敗しました")
+console.assert(wishToSuccess(), "失敗しました");
+
+// log
+console.info('info');
+console.warn('warn');
+console.error('error');
+
 
 console.log('=====End=====');
